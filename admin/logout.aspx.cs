25c9cd899201afa0275533079180a0311649e7b5
Rinkal Gadhiya <rinkalgadhiya13@gmail.com>
2024-11-21 18:00:48 +0530

    Add files via upload

---
+++ admin/logout.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class admin_logout : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["admin"] == null)
+        {
+            Session.Abandon();
+            Session.RemoveAll();
+            Response.Redirect("~/index.aspx");
+        }
+        
+    }
+}
